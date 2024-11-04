b630ea913a233461cfd118ed1194c1ca5531b75c
Daffa Randika <daffa.randika.3@gmail.com>
2024-11-04 15:07:28 +0700

    frontend view

--- frontend/src/components/ui/calculator-sidebar.tsx
+++ frontend/src/components/ui/calculator-sidebar.tsx
@@ -1,9 +1,9 @@
-import { X, Plus } from "lucide-react"
-import { Point } from "../model/types"
-import { useState } from "react"
-
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Point } from "../model/types"
+import { X } from "lucide-react"
+import { useState } from "react"
+
 import {
   Sidebar,
   SidebarContent,
@@ -11,9 +11,20 @@ import {
   SidebarProvider,
 } from '@/components/ui/sidebar'
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 export function CalculatorSidebar() {
 
   const [points, setPoints] = useState<Point[]>([{ x: '', y: ''}])
+  const [x, setX] = useState("")
 
   const handlePointChanges = (index: number, axis: 'x'|'y', value: string) => {
     const newPoints = [...points];
@@ -30,55 +41,87 @@ export function CalculatorSidebar() {
     setPoints(newPoints.length ? newPoints : [{ x: '', y: ''}])
   }
 
+  const handleSubmit = () => {
+  }
+
   return (
     <SidebarProvider>
-      <Sidebar className="w-60">
-        <SidebarHeader className="border-b border-border px-4 py-2">
-          <h2 className="text-lg font-semibold">Interpolasi</h2>
-        </SidebarHeader>
-        <SidebarContent>
-          <div className="flex flex-col gap-2 p-4">
-            {points.map((point, index) => (
-              <div key={index} className="flex items-center gap-2">
-                <div className="flex flex-grow items-center gap-2">
-                  <Input
-                    value={point.x}
-                    onChange={(e) => handlePointChanges(index, 'x', e.target.value)}
-                    placeholder="x"
-                    className="w-20"
-                    type="number"
-                    aria-label={`X coordinate for point ${index + 1}`}
-                  />
-                  <Input
-                    value={point.y}
-                    onChange={(e) => handlePointChanges(index, 'y', e.target.value)}
-                    placeholder="y"
-                    className="w-20"
-                    type="number"
-                    aria-label={`Y coordinate for point ${index + 1}`}
-                  />
-                </div>
-                {index !== points.length - 1 && (
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={() => handleRemovePoint(index)}
-                    aria-label={`Remove point ${index + 1}`}
-                  >
-                    <X className="h-4 w-4" />
-                  </Button>
-                )}
+      <form onSubmit={handleSubmit}>
+        <Sidebar className="w-80">
+          <SidebarHeader className="border-b border-border px-4 py-2">
+            <h2 className="text-lg font-semibold">Interpolasi</h2>
+          </SidebarHeader>
+          <SidebarContent>
+            <div className="flex flex-col gap-2 p-4">
+              <div className="flex flex-row items-center">
+                <label htmlFor="x" className="text-left w-4/5">Metode interpolasi: </label>
+                <Select>
+                  <SelectTrigger className="w-1/2">
+                    <SelectValue placeholder="Pilih metode" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Metode</SelectLabel>
+                      <SelectItem value="newton">Newton</SelectItem>
+                      <SelectItem value="lagrange">Lagrange</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
               </div>
-            ))}
-            <Button
-              variant="outline"
-              className="mt-2"
-              onClick={() => {}}
-            >Hitung
-            </Button>
-          </div>
-        </SidebarContent>
-      </Sidebar>
+              <div className="flex flex-row items-center">
+                <label htmlFor="x" className="text-left w-4/5">Nilai yang ingin diinterpolasi:</label>
+                <Input
+                  value={x}
+                  onChange={(e) => {setX(e.target.value)}}
+                  placeholder="x"
+                  className="w-1/5 text-left"
+                  type="number"
+                  aria-label={`input x`}
+                />
+              </div>
+              {points.map((point, index) => (
+                <div key={index} className="flex items-center gap-2">
+                  <div className="flex flex-grow items-center gap-2">
+                    <Input
+                      value={point.x}
+                      onChange={(e) => handlePointChanges(index, 'x', e.target.value)}
+                      placeholder={ `x_${index}` }
+                      className="w-2/5"
+                      type="number"
+                      aria-label={`X coordinate for point ${index + 1}`}
+                    />
+                    <Input
+                      value={point.y}
+                      onChange={(e) => handlePointChanges(index, 'y', e.target.value)}
+                      placeholder={ `y_${index}` }
+                      className="w-2/5"
+                      type="number"
+                      aria-label={`Y coordinate for point ${index + 1}`}
+                    />
+                  {index !== points.length - 1 && (
+                    <Button
+                      className={ "w-1/5" + (index !== points.length-1 ? "invisible" : "visible") }
+                      variant="ghost"
+                      size="icon"
+                      onClick={() => handleRemovePoint(index)}
+                      aria-label={`Remove point ${index + 1}`}
+                    >
+                      <X className="h-4 w-4" />
+                    </Button>
+                  )}
+                  </div>
+                </div>
+              ))}
+              <Button
+                variant="outline"
+                className="mt-2"
+                onClick={() => {}}
+              >Hitung
+              </Button>
+            </div>
+          </SidebarContent>
+        </Sidebar>
+      </form>
     </SidebarProvider>
   )
 }
